bd0e02a27c52755d9877de7b32b30cc52d887298
Junhyun Yang <mrdotori@gmail.com>
2021-04-15 14:30:02 +0900

    first update

---
+++ android/app/src/main/kotlin/com/example/geolocator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.whenion.geolocatorapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My First Weather',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Weather'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _locality = '';
+  String _weather = '';
+
+  @override
+  void initState() {
+    super.initState();
+    getPosition().then((position) {
+      getPlacemark(position.latitude, position.longitude).then((data) {
+        getData(position.latitude, position.longitude).then((weather) {
+          setState(() {
+            _locality = data.locality;
+            _weather = weather;
+          });
+        });
+      });
+    });
+  }
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_locality',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              '$_weather',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<Position> getPosition() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    return position;
+  }
+
+  Future<Placemark> getPlacemark(double latitude, double longitude) async {
+    List<Placemark> placemark =
+        await placemarkFromCoordinates(latitude, longitude);
+    return placemark[0];
+  }
+
+  Future<String> getData(double latitude, double longitude) async {
+    String api = 'http://api.openweathermap.org/data/2.5/forecast';
+    String appId = '54f3bc65289f73ba37aa18bfb39beb4f';
+
+    String url = '$api?lat=$latitude&lon=$longitude&APPID=$appId';
+
+    http.Response response = await http.get(Uri.parse(url));
+
+    Map parsed = json.decode(response.body);
+
+    return parsed['list'][0]['weather'][0]['description'];
+  }
+}
